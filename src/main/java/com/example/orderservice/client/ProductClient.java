2712d321ab36df82481d40650cacfa9132ad5c24
mhdfaris <mhdfaris.farhan@gmail.com>
2025-12-23 15:04:17 +0800

    test

---
+++ src/main/java/com/example/orderservice/client/ProductClient.java
@@ -0,0 +1,4 @@
+package com.example.orderservice.client;
+
+public class ProductClient {
+}
---
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,5 @@
+package com.example.orderservice.controller;
+package com.example.orderservice.service.OrderService
+
+public class OrderController {
+}
